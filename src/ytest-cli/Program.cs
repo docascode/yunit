4a40fa1482a7cfa262e3873063255760b56b53ed
Yufei Huang <yufeih@live.com>
2019-07-09 21:37:18 +0800

    Initial commit

---
+++ src/ytest-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ytest_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ytest/TestRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace ytest
+{
+    public class VsTestRunner : ITestDiscoverer, ITestExecutor
+    {
+        public void Cancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
