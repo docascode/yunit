03c4c261ca78c23ea2333e50cf534f4f86ccb8fd
Yufei Huang <yufeih@live.com>
2019-11-25 13:14:28 +0800

    Remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#bin/bash
-rm -f $PWD/drop
-dotnet pack src/yunit -c Release -o $PWD/drop
-dotnet test -c Release
-dotnet test test/yunit.nuget.test -c Release
-if [ ! -f $PWD/test/yunit.nuget.test/bin/Release/netcoreapp3.0/foo ]; then
-    echo "yunit.nuget.test failed" 1>&2
-    exit 1
-fi
