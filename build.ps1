5eab5ebebc499b261ab97f59a19bcbaa5626f3d5
Yufei Huang <yufeih@live.com>
2020-09-01 15:29:13 +0800

    Include yunit.nuget.test.csproj in the solution

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Remove-Item drop -Force -Recurse -ErrorAction Ignore
 dotnet pack src/yunit -c Release -o $PSScriptRoot/drop
 dotnet test -c Release
-dotnet test test/yunit.nuget.test -c Release
+dotnet test test/yunit.nuget.test -c NuGetTest
 if (-not (Test-Path -Path "$PSScriptRoot\test\yunit.nuget.test\bin\Release\netcoreapp3.0\foo")) {
     throw 'yunit.nuget.test failed'
 }
