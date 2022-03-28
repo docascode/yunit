b13923fda09927a3ab80f0fc9b2ef46fa7b8ef9c
Yufei Huang <yufeih@live.com>
2022-03-28 15:15:25 +0800

    Fix build

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ Remove-Item drop -Force -Recurse -ErrorAction Ignore
 dotnet pack src/yunit -c Release -o $PSScriptRoot/drop
 dotnet test -c Release
 dotnet test test/yunit.nuget.test -c NuGetTest
-if (-not (Test-Path -Path "$PSScriptRoot\test\yunit.nuget.test\bin\Release\net5.0\foo")) {
+if (-not (Test-Path -Path "$PSScriptRoot\test\yunit.nuget.test\bin\Release\net6.0\foo")) {
     throw 'yunit.nuget.test failed'
 }
